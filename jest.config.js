7b2f609060d5247fb9da1c4c32124c8fbadeb97b
BenMiriello <benmiriello@gmail.com>
2025-06-20 12:26:27 -0700

    install jest

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/packages'],
+  testMatch: [
+    '**/__tests__/**/*.ts',
+    '**/?(*.)+(spec|test).ts'
+  ],
+  collectCoverageFrom: [
+    'packages/*/src/**/*.ts',
+    '!packages/*/src/**/*.d.ts'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  projects: [
+    {
+      displayName: '@storymark/core',
+      testMatch: ['<rootDir>/packages/core/**/*.(test|spec).ts'],
+      preset: 'ts-jest',
+      testEnvironment: 'node'
+    },
+    {
+      displayName: '@storymark/react',
+      testMatch: ['<rootDir>/packages/react/**/*.(test|spec).ts'],
+      preset: 'ts-jest',
+      testEnvironment: 'jsdom'
+    }
+  ]
+};
